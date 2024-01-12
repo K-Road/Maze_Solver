5045906060df2313411f9e95c3d1bea67200a2a1
K-Road <game@chrodeprime.com>
2024-01-13 01:36:57 +1100

    Added draw_move

--- cell.py
+++ cell.py
@@ -13,6 +13,8 @@ class Cell:
         self._win = window
 
     def draw(self, x1,y1,x2,y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -29,3 +31,44 @@ class Cell:
         if self.has_bottom_wall:
             wall = Line(Point(x1,y2),Point(x2,y2))
             self._win.draw_line(wall)
+
+    def draw_move(self, to_cell, undo=False): #adjacent cell move
+        if self._win is None:
+            return
+        colour = "red"
+        if undo:
+            colour = "gray"
+
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        t_x_mid = (to_cell._x1 + to_cell._x2) /2
+        t_y_mid = (to_cell._y1 + to_cell._y2) /2
+
+        #left
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1,y_mid), Point(x_mid,y_mid))
+            self._win.draw_line(line,colour)
+            line = Line(Point(t_x_mid,t_y_mid), Point(to_cell._x2, t_y_mid))
+            self._win.draw_line(line,colour)
+
+        #right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid,y_mid), Point(self._x2,y_mid))
+            self._win.draw_line(line,colour)
+            line = Line(Point(to_cell._x1, t_y_mid), Point(t_x_mid,t_y_mid))
+            self._win.draw_line(line,colour)
+        
+        #up
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line,colour)
+            line = Line(Point(t_x_mid,to_cell._y2), Point(t_x_mid,t_y_mid))
+            self._win.draw_line(line,colour)
+        
+        #down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid,y_mid),Point(x_mid,self._y2))
+            self._win.draw_line(line,colour)
+            line = Line(Point(t_x_mid,t_y_mid),Point(t_x_mid,to_cell._y1))
+            self._win.draw_line(line,colour)
--- main.py
+++ main.py
@@ -5,19 +5,29 @@ def main():
     win = Window(800, 600)
     line1 = Line(Point(1,1),Point(200,200))
     #win.draw_line(Line(Point(10,10),Point(400,100)),"red")
-    win.draw_line(line1, "red")
+  #  win.draw_line(line1, "red")
 
 
     
     cell1 = Cell(win) #Point(20,20),Point(500,500),True,True,True,True)
     #cell2 = Cell(win,50,50,200,200,True,True,True,True)
-    cell1.draw(50,50,300,300)
+    cell1.draw(150,150,200,200)
     
 
     cell2 = Cell(win) #Point(20,20),Point(500,500),True,True,True,True)
     #cell2 = Cell(win,50,50,200,200,True,True,True,True)
-    cell2.draw(400,50,500,400)
+    cell2.draw(400,200,500,300)
     
+    cell1.draw_move(cell2,True)
+
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(10,10,50,50)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.draw(50,10,100,50)
+
+    c1.draw_move(c2)
     
     win.wait_for_close()
 
