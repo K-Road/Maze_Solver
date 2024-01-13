b8b9ea01ef2182a5b545773db38a0dec4b962da0
K-Road <game@chrodeprime.com>
2024-01-13 20:26:11 +1100

    Added break_walls_r

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 from graphics import Line, Point
 from cell import Cell
 import time
+import random
 
 class Maze:
     def __init__(
@@ -12,6 +13,7 @@ class Maze:
         cell_size_x,
         cell_size_y,
         window=None,
+        seed=None,
     ):
         self._x1 = x1
         self._y1 = y1
@@ -21,8 +23,13 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = window
         self._cells = []
+        if seed:
+            random.seed(seed)
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
+        
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -59,12 +66,50 @@ class Maze:
         self._cells[self._num_cols-1][self._num_rows-1].has_bottom_wall = False
         self._draw_cell(self._num_cols-1,self._num_rows-1)
 
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            #check L (West)
+            if i > 0 and self._cells[i-1][j].visited == False:
+                    to_visit.append((i-1, j,'L'))
+            #check R (East)
+            if i < self._num_cols-1 and self._cells[i+1][j].visited == False:
+                    to_visit.append((i+1,j,'R'))
+            #check T (North)
+            if j > 0 and self._cells[i][j-1].visited == False:
+                    to_visit.append((i,j-1,'T'))
+            #check B (South)
+            if j < self._num_rows-1 and self._cells[i][j+1].visited == False:
+                    to_visit.append((i,j+1,'B'))
+            if len(to_visit) == 0:
+                self._draw_cell(i,j)
+                return
+            direction = random.randrange(len(to_visit))
+            next_cell = to_visit[direction]
+
+            if next_cell[2] == 'L':
+                self._cells[i][j].has_left_wall = False
+                self._cells[next_cell[0]][next_cell[1]].has_right_wall = False
+            if next_cell[2] == 'R':
+                self._cells[i][j].has_right_wall = False
+                self._cells[next_cell[0]][next_cell[1]].has_left_wall = False
+            if next_cell[2] == 'T':
+                self._cells[i][j].has_top_wall = False
+                self._cells[next_cell[0]][next_cell[1]].has_bottom_wall = False
+            if next_cell[2] == 'B':
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[next_cell[0]][next_cell[1]].has_top_wall = False
+            #self._cells[i][j].draw_move(self._cells[to_visit[direction][0]],[to_visit[direction][1]])
+            self._break_walls_r(next_cell[0],next_cell[1])
+            
+        
 
     def _animate(self):
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.05)
+        time.sleep(0.005)
 
         
             
