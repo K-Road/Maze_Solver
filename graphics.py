d07f52b03077b0bb24c904524db6ef0e97cdb2a9
K-Road <game@chrodeprime.com>
2024-01-13 15:11:38 +1100

    added height width

--- graphics.py
+++ graphics.py
@@ -2,8 +2,8 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-      #  self.width = width
-      #  self.height = height
+        self.width = width
+        self.height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
